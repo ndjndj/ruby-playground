0d6d34d6234187d2ad57d7c8a99c5dabb2cce3f2
ndjndj <teq.ndj@gmail.com>
2023-04-18 00:53:42 +0900

    f

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "application#hello"
+  #root "application#hello"
+  root "application#good_bye"
 end
