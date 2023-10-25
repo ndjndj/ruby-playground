a6c4d3939e974d6160dbf4248cdb3dbe09c41132
ndjndj <teq.ndj@gmail.com>
2023-10-25 19:02:44 +0900

    f

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def hello 
         render html: "<h1>HEEEELLLLLLLLOOOOOOO</h1>"
     end
+
+    def goodbye
+        render html:"good bye"
+    end
 end
