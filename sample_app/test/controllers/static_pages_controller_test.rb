3d176f27afcbcc2a128c40c57fe51d83110be8aa
ndjndj <teq.ndj@gmail.com>
2023-11-01 19:33:52 +0900

    f

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -21,4 +21,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+
+  end
+
+  test "should get contact" do
+    get static_pages_contac_url
+    assert_response :success
+
+  end
 end
