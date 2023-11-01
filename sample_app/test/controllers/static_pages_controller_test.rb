d6f34aca16bf6c0edf55ffbefe91ad0de177cb41
ndjndj <teq.ndj@gmail.com>
2023-11-01 19:36:03 +0900

    f

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -25,12 +25,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-
   end
 
-  test "should get contac" do
-    get static_pages_contact_url
-    assert_response :success
-
-  end
 end
