5593b29ec964b671b0dc9465b8dba592d76201db
ndjndj <teq.ndj@gmail.com>
2023-11-20 12:38:46 +0900

    f

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/sighup' to: 'users#new'
+  get '/sighup',  to: 'users#new'
 end
