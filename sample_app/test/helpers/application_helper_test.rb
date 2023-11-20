cf78187149477c78f267decca98dd3f8b16876dc
ndjndj <teq.ndj@gmail.com>
2023-11-20 12:32:09 +0900

    f

--- sample_app/test/helpers/application_helper_test.rb
+++ sample_app/test/helpers/application_helper_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal "Ruby on Rails Tutorial Sample App", full_title
-    assert_equal "Ruby on Rails Tutorial Sample App | Help", full_title("Help")
+    assert_equal "Help | Ruby on Rails Tutorial Sample App", full_title("Help")
   end
 end
