08e1d24ed7eff0cb88ce273390ad1f86deb3ccc6
ndjndj <teq.ndj@gmail.com>
2023-11-21 12:25:08 +0900

    f

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
