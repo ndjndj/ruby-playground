5bbe39f65ff4e71e27362f3b6d435f46b5b4f9ee
ndjndj <teq.ndj@gmail.com>
2023-11-22 19:13:57 +0900

    f

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -20,3 +20,14 @@ class ActiveSupport::TestCase
     session[:user_id] = user.id
   end
 end
+
+
+class ActionDispatch::IntergrationTest
+  def log_in_as(user, password: "passwordpassword", remember_me: "1")
+    post login_path, params: { session: {
+      email: user.email,
+      password: password,
+      remember_me: remember_me
+    } }
+  end
+end
