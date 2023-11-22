0031f4ad84719bcb5d1a7a71bb9c249b66475524
ndjndj <teq.ndj@gmail.com>
2023-11-22 14:28:02 +0900

    f

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+
   private
     def user_params
       params.require(:user).permit(
