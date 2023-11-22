04b80871576633c556742bff73428ae0acd738ed
ndjndj <teq.ndj@gmail.com>
2023-11-22 13:33:46 +0900

    f

--- sample_app/app/javascript/custom/menu.js
+++ sample_app/app/javascript/custom/menu.js
@@ -1,18 +1,24 @@
 // メニュー操作
 
+// トグルリスナーを追加する
+function addToggleListener(selectedId, menuId, toggleClass) {
+    const selectedElem = document.querySelector(`#${selectedId}`);
+    selectedElem.addEventListener(
+        "click",
+        function(event) {
+            event.preventDefault();
+            const menu = document.querySelector(`#${menuId}`);
+            menu.classList.toggle(toggleClass);
+        }
+    )
+}
+
 // トグルリスナーを追加して Click event を listen する
 document.addEventListener(
     "turbo:load",
     function() {
-        let hamburger = document.querySelector("#hamburger");
-        hamburger.addEventListener(
-            "click",
-            function(event) {
-                event.preventDefault();
-                let menu = document.querySelector("#navbar-menu")
-                menu.classList.toggle("collapse")
-            }
-        );
+        addToggleListener("hamburger", "navbar-menu"  , "collapse");
+        addToggleListener("account"  , "dropdown-menu", "active");
 
         let account = document.querySelector("#account");
         account.addEventListener(
