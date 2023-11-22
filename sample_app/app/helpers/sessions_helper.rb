b84c49464d252ee74184c3c84ce4af04a6394a8c
ndjndj <teq.ndj@gmail.com>
2023-11-22 17:51:48 +0900

    f

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def log_out
     reset_session
     @current_user = nil
