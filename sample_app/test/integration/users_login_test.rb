b57dc8923fe93bdf389b1d6336a69415ff533c7a
ndjndj <teq.ndj@gmail.com>
2023-11-22 13:58:53 +0900

    f

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -22,5 +22,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
       email: @user.email,
       password: "passwordpassword"
     } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/new"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
