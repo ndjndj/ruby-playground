345c42a12c9f6b626477c349b77d4c23e2b12934
ndjndj <teq.ndj@gmail.com>
2023-11-22 13:23:26 +0900

    f

--- sample_app/app/javascript/custom/menu.js
+++ sample_app/app/javascript/custom/menu.js
@@ -4,6 +4,16 @@
 document.addEventListener(
     "turbo:load",
     function() {
+        let hamburger = document.querySelector("#hamburger");
+        hamburger.addEventListener(
+            "click",
+            function(event) {
+                event.preventDefault();
+                let menu = document.querySelector("#navbar-menu")
+                menu.classList.toggle("active")
+            }
+        );
+
         let account = document.querySelector("#account");
         account.addEventListener(
             "click",
