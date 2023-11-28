3588f4dc50e9251afbbb0435ac1d77d520c519b9
ndjndj <teq.ndj@gmail.com>
2023-11-28 11:23:06 +0900

    f

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  validates :password,
+    presence: true,
+    length: {minimum: 6},
+    allow_nil: true
 
   class << self
     def digest(string)
