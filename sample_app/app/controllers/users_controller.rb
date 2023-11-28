af4bf4ffc187ed771bc781e625cad79001c413d0
ndjndj <teq.ndj@gmail.com>
2023-11-28 11:41:22 +0900

    f

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -39,4 +39,11 @@ class UsersController < ApplicationController
         :name, :email, :password, :password_confirmation
       )
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
