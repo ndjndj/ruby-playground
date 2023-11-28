8e7f03ca2e8fb5ed8e6e7caa69d787ba7a63157a
ndjndj <teq.ndj@gmail.com>
2023-11-28 11:11:23 +0900

    f

--- sample_app/test/integration/users_edit_test.rb
+++ sample_app/test/integration/users_edit_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    patch user_path(@user), params: {user: {
+      name: "",
+      password: "foo",
+      password_confirmation: "bar"
+    }}
+  end
 end
