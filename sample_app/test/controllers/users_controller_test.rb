2a673e32e863dfa2d77bba35feaaaf5221d59177
ndjndj <teq.ndj@gmail.com>
2023-11-28 11:54:17 +0900

    f

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
   test "should get new" do
     get signup_path
     assert_response :success
