b2bc8e3054b51cc8b9e2c8814b4490f64afb8559
ndjndj <teq.ndj@gmail.com>
2023-11-28 12:04:14 +0900

    f

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -33,5 +33,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: {user: {
+      name: @user.name,
+      email: @user.email
+    }}
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
