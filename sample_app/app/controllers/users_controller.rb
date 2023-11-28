2b4e79ba6b827efda1d681d4e51c78412e7172c0
ndjndj <teq.ndj@gmail.com>
2023-11-28 12:09:58 +0900

    f

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -47,4 +47,9 @@ class UsersController < ApplicationController
         redirect_to login_url, status: :see_other
       end
     end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless @user == current_user
+    end
 end
