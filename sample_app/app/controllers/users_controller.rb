cae9747e69d16c1840ec183dcd7839e912eb877f
ndjndj <teq.ndj@gmail.com>
2023-11-28 12:13:46 +0900

    f

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -23,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile uploaded"
       redirect_to @user
