c2559344c972453d527bbe13e6f0f8f3af86729f
ndjndj <teq.ndj@gmail.com>
2023-11-29 13:36:28 +0900

    f

--- sample_app/test/integration/users_index_test.rb
+++ sample_app/test/integration/users_index_test.rb
@@ -1,33 +1,44 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
   end
+end
 
-  test "index as admin including pagination and delete links" do
+class UsersIndexAdmin < UsersIndex
+  def setup
+    super
     log_in_as(@admin)
     get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination', count: 2
-    first_page_of_users = User.paginate(page: 1)
+  end
+end
+
+class UsersIndexTest < UsersIndexAdmin
+  test "should be render the index page" do
+    asssert_template "users/index"
+  end
+
+  test "should paginate users" do
+    assert_select "div.pagination"
+  end
+
+  test "should have delete links" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
       unless user == @admin
         assert_select "a[href=?]", user_path(user), text: "delete"
       end
     end
-    assert_difference "User.count", -1 do
-      delete user_path(@non_admin)
-      assert_response :see_other
-      assert_redirected_to users_url
-    end
   end
 
-  test "index as non-admin" do
-    log_in_as(@non_admin)
-    get users_path
-    assert_select "a", text: "delete", count: 0
+  test "should be able to delete non-admin user" do
+    assert_difference "User.count", -1 do
+      delete user_path(@non_admin)
+    end
+    assert_response :see_other
+    assert_redirected_to users_url
   end
 end
