d440ebff0c4006668e86ef1c73dea0103cebdf9a
ndjndj <teq.ndj@gmail.com>
2023-11-29 13:06:39 +0900

    f

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user&.authenticate(params[:session][:password])
-      if @user.activated?
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      if user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
-        params[:session][:remember_me] == '1' ? remember(@user)
-                                              : forget(@user)
-        log_in @user
-        redirect_to forwarding_url || @user
+        params[:session][:remember_me] == '1' ? remember(user)
+                                              : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
       else
         message = "Account not activated"
         message += "Check your email for the activation link."
