8da9f89baea4843b16e2cc4f647dfa65ef858845
ndjndj <teq.ndj@gmail.com>
2023-11-30 11:51:34 +0900

    f

--- sample_app/test/integration/password_resets_test.rb
+++ sample_app/test/integration/password_resets_test.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
+class PasswordResets < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+end
+
 class PasswordResetsTest < ActionDispatch::IntegrationTest
-  
+
 end
