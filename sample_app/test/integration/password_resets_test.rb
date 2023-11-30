7be6615dfb8d40f73f87fd8b7ac6952e0c10b5d2
ndjndj <teq.ndj@gmail.com>
2023-11-30 11:52:50 +0900

    f

--- sample_app/test/integration/password_resets_test.rb
+++ sample_app/test/integration/password_resets_test.rb
@@ -6,6 +6,14 @@ class PasswordResets < ActionDispatch::IntegrationTest
   end
 end
 
+class ForgotPasswordFormTest < PasswordResets
+  test "password reset path" do
+    get new_password_reset_path
+    assert_template "password_resets/new"
+    assert_select "input[name=?]", "password_reset[email]"
+  end
+end
+
 class PasswordResetsTest < ActionDispatch::IntegrationTest
 
 end
