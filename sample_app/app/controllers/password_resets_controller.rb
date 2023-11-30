ae07aeef917e8ecd320245f14ca906ac887328d4
ndjndj <teq.ndj@gmail.com>
2023-11-30 11:04:11 +0900

    f

--- sample_app/app/controllers/password_resets_controller.rb
+++ sample_app/app/controllers/password_resets_controller.rb
@@ -22,6 +22,20 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render "edit", status: :unprocessable_entity
+    elsif @user.update(user_params)
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
   private
     def get_user
       @user = User.find_by(email: params[:email])
