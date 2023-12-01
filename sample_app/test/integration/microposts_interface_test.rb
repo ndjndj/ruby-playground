8514125b4f78838ed3cbf29d89e5d362ccd11fac
ndjndj <teq.ndj@gmail.com>
2023-12-01 13:32:32 +0900

    f

--- sample_app/test/integration/microposts_interface_test.rb
+++ sample_app/test/integration/microposts_interface_test.rb
@@ -1,5 +1,22 @@
 require "test_helper"
 
-class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-  
+class MicropostsInterface < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+end
+
+class MicropostsInterfaceTest < MicropostsInterface
+
+  test "should paginate microposts" do
+    get root_path
+    assert_select "div.pagination"
+  end
+
+  test "should show errors but not create micropost on invalid submission" do
+    assert_no_difference "Micropost.count" do
+      post microposts_path, params: {micropost: {content: ""}}
+    end
+  end
 end
