4aaa8f0d2e415ba821e210069a0f8b0f6b2ce895
ndjndj <teq.ndj@gmail.com>
2023-12-01 18:56:50 +0900

    f

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -82,6 +82,18 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
     def downcase_email
       self.email.downcase!
