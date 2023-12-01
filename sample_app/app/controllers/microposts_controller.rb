5fc242eea165cfc6a6db8f4a3538cb64cdf4e94c
ndjndj <teq.ndj@gmail.com>
2023-12-01 10:08:42 +0900

    f

--- sample_app/app/controllers/microposts_controller.rb
+++ sample_app/app/controllers/microposts_controller.rb
@@ -1,8 +1,14 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  
-  def create
 
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render "static_pages/home", status: :unprocessable_entity
+    end
   end
 
   def destroy
