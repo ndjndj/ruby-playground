2f143985190e7340b1921ae22602faffe6115cb1
ndjndj <teq.ndj@gmail.com>
2023-12-01 16:54:29 +0900

    f

--- sample_app/app/javascript/custom/image_upload.js
+++ sample_app/app/javascript/custom/image_upload.js
@@ -1,7 +1,7 @@
 function alertImage(event) {
     let img_upload = document.querySelector("#micropost_image")
     const size_in_megabytes = img_upload.files[0].size/1024/1024
-    if (size_in_megabytes) {
+    if (size_in_megabytes > 5) {
         alert("Maximum file size is 5 MB. Please choose a smaller file")
         img_upload = "";
     }
