ecb3b36acaba1f3f280047b289a72f258951a6c2
ndjndj <teq.ndj@gmail.com>
2023-12-01 19:00:40 +0900

    f

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -101,6 +101,7 @@ class UserTest < ActiveSupport::TestCase
   test "should follow and unfollow a user" do
     michael = users(:michael)
     archer = users(:archer)
+    debugger
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
