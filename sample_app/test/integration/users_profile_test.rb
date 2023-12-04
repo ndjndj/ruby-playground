b5dc4c17d0e04cac8c11f59c8185a2c0ada1f830
ndjndj <teq.ndj@gmail.com>
2023-12-04 12:09:00 +0900

    f

--- sample_app/test/integration/users_profile_test.rb
+++ sample_app/test/integration/users_profile_test.rb
@@ -18,7 +18,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
-    assert_match "#{@user.following.count.to_s}following", response.body
-    assert_match "#{@user.followers.count.to_s}followers", response.body
+    assert_match "#{@user.following.count.to_s}", response.body
+    assert_match "#{@user.followers.count.to_s}", response.body
   end
 end
