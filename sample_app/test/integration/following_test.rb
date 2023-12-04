d5f82743519c5fdc87f099aefa1c410e60852806
ndjndj <teq.ndj@gmail.com>
2023-12-04 12:50:12 +0900

    f

--- sample_app/test/integration/following_test.rb
+++ sample_app/test/integration/following_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
-class FollowingTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class Following < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+end
+
+class FollowingTest < Following
+  test "following page" do
+    get following_user_path(@user)
+    assert_response :success
+    assert_not @user.following.empty?
+    assert_match @user.following.count.to_s, response.body
+    @user.following.each do |user|
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
+
 end
