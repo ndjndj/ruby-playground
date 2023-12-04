e6e4c24debe61c7dbf4f0fa471d997a8c1202bf4
ndjndj <teq.ndj@gmail.com>
2023-12-04 18:31:25 +0900

    f

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -86,6 +86,7 @@ class User < ApplicationRecord
     part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
     Micropost.left_outer_joins(user: :followers)
              .where(part_of_feed, {id: id})
+             .distinct
              .includes(:user, image_attachment: :blob)
   end
 
