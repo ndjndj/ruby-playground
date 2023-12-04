a2d64b1714fa6d47a89a2a231808d4fe73c20c14
ndjndj <teq.ndj@gmail.com>
2023-12-04 18:25:51 +0900

    f

--- sample_app/test/integration/following_test.rb
+++ sample_app/test/integration/following_test.rb
@@ -75,7 +75,7 @@ class FollowingPagesTest < Following
   test "feed on Home page" do
     get root_path
     @user.feed.paginate(page: 1).each do |micropost|
-      assert_match CGI.escapeHTML('id="micropost-'), micropost
+      assert_match CGI.escapeHTML(micropost.content), response.body
     end
   end
 end
