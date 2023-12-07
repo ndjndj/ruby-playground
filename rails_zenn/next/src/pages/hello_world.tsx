48cb87013b3d17ec2741ddde9eef9cfe01fa9fcd
ndjndj <teq.ndj@gmail.com>
2023-12-07 17:42:24 +0900

    f

--- rails_zenn/next/src/pages/hello_world.tsx
+++ rails_zenn/next/src/pages/hello_world.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from 'next'
 
 const HelloWorld: NextPage = () => {
     return (
-        <div>
+        <>
             <h1>Title</h1>
             <p>content</p>
-        </div>
+        </>
     )
 }
 
