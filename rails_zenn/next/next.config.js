0dd4982b752c20ac8ee8e273d048124b2556e506
ndjndj <teq.ndj@gmail.com>
2023-12-07 17:43:13 +0900

    f

--- rails_zenn/next/next.config.js
+++ rails_zenn/next/next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   // hot reload
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1200, //チェック時間
-      aggregateTimeout: 500, // 遅延時間
-      ignored : ['node_modules']
-    };
-    return config;
-  },
+  //webpackDevMiddleware: (config) => {
+  //  config.watchOptions = {
+  //    poll: 1200, //チェック時間
+  //    aggregateTimeout: 500, // 遅延時間
+  //    ignored : ['node_modules']
+  //  };
+  //  return config;
+  //},
 }
 
 module.exports = nextConfig
