cfd969f1f814bf29440822f8ccc779556fb975a7
ndjndj <teq.ndj@gmail.com>
2023-12-08 12:57:35 +0900

    f

--- rails_zenn/next/src/pages/hello_mui.tsx
+++ rails_zenn/next/src/pages/hello_mui.tsx
@@ -1,16 +1,21 @@
+import { css } from '@emotion/react'
 import { Button } from '@mui/material'
 import type { NextPage } from 'next'
+
+const buttonCss = css({
+    padding: '24px'
+})
 
 const HelloMui: NextPage = () => {
     return (
         <>
-            <Button variant='contained' sx={{ p: '24px' }}>
+            <Button variant='contained' css={buttonCss}>
                 Button1
             </Button>
-            <Button variant='outlined' sx={{ p: '24px' }}>
+            <Button variant='outlined' css={buttonCss}>
                 Button2
             </Button>
-            <Button variant='contained' color="error" sx={{ p: '24px' }}>
+            <Button variant='contained' color="error" css={buttonCss}>
                 Button3
             </Button>
         </>
