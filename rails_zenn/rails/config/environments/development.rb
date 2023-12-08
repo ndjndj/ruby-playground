c7dd82ce57fa06ecc97d395936306233a264a180
ndjndj <teq.ndj@gmail.com>
2023-12-08 15:35:34 +0900

    f

--- rails_zenn/rails/config/environments/development.rb
+++ rails_zenn/rails/config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_options = { from: "no-replay@example.com" }
   config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
