36ce0d5b67e1cc5b8019b10900aee4fbaac98139
ndjndj <teq.ndj@gmail.com>
2023-12-11 12:59:46 +0900

    ｆ

--- rails_zenn/rails/app/serializers/article_serializer.rb
+++ rails_zenn/rails/app/serializers/article_serializer.rb
@@ -14,7 +14,21 @@ class ArticleSerializer < ActiveModel::Serializer
             - ((now.day > created_at.day) ? 0 : 1)
     years = months.div(12)
 
-  return "#{years}年前" if years > 0
-  return "#{months}ヶ月前" if months > 0
+    return "#{years}年前" if years > 0
+    return "#{months}ヶ月前" if months > 0
+
+    seconds = (Time.zone.now - object.created_at).round
+
+    days = seconds / (60 * 60 * 24)
+    return "#{days}日前" if days.positive?
+
+    hours = seconds / (60 * 60)
+    return "#{hours}時間前" if hours.positive?
+
+    minutes = seconds / 60
+    return "#{minutes}分前" if minutes.positive?
+
+    "#{seconds}秒前"
+
   end
 end
