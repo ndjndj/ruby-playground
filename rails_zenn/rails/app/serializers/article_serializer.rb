10e763f3587a0537ac6d879599f113d2718424aa
ndjndj <teq.ndj@gmail.com>
2023-12-11 12:55:06 +0900

    f

---
+++ rails_zenn/rails/app/serializers/article_serializer.rb
@@ -0,0 +1,17 @@
+class ArticleSerializer < ActiveModel::Serializer
+  attributes :id, :title, :content, :created_at, :from_today
+
+  def created_at
+    object.created_at.strftime("%Y/%m/%d")
+  end
+
+  def from_today
+    now = Time.zone.now
+    created_at = object.created_at
+
+    months = (now.year - created_at.year) * 12
+            + now.month - created_at.month
+            - ((now.day > created_at.day) ? 0 : 1)
+    years = months.div(12)
+  end
+end
