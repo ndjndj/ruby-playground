23776d41d7fcea998fa55aa7e48b4a8b2ce5d477
ndjndj <teq.ndj@gmail.com>
2023-12-11 18:30:48 +0900

    f

--- rails_zenn/rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails_zenn/rails/app/controllers/api/v1/current/articles_controller.rb
@@ -12,4 +12,9 @@ class Api::V1::Current::ArticlesController < Api::V1::BaseController
     article.update!(article_params)
     render json: article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :status)
+    end
 end
