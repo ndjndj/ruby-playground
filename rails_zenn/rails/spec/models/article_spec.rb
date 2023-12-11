40fc5240d8ec929984c642103cbc03eacd476012
ndjndj <teq.ndj@gmail.com>
2023-12-11 12:39:51 +0900

    f

--- rails_zenn/rails/spec/models/article_spec.rb
+++ rails_zenn/rails/spec/models/article_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "factory のデフォルト設定に従ったとき" do
+    subject { create(:article) }
+
+    it "正常にレコードを新規作成することができる" do
+      expect { subject }.to change { Article.count }.by(1)
+    end
+  end
+
+  describe "Validations" do
+    subject { article.valid? }
+
+    let(:article) { build(:article, title: , content: , status: , user:) }
+    let(:title) { Faker::Lorem.sentence }
+    let(:content) { Faker::Lorem.paragraph }
+    let(:status) { :published }
+    let(:user) { create(:user) }
+
+    context "全ての値が正常なとき" do
+      it "検証が通る" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "ステータスが公開済みかつ、タイトルが空のとき" do
+      let(:title) { "" }
+
+      it "エラーメッセージが返る" do
+        expect(subject).to be_falsy
+        expect(article.errors.full_messages).to eq ["本文を入力してください"]
+      end
+    end
+
+    context "ステータスが未保存かつ、すでに同一ユーザーが未保存ステータスの記事を所有していたとき" do
+      let(:status) { :unsaved }
+      before { create(:article), status: :unsaved, user: }
+
+      it "例外が発生する" do
+        expect { subject }.to raise_error(StandardError)
+      end
+    end
+  end
 end
