f0ea61e609cbb9064f52109db62dd9ddd469fa60
ndjndj <teq.ndj@gmail.com>
2023-12-11 13:21:32 +0900

    f

--- rails_zenn/rails/config/routes.rb
+++ rails_zenn/rails/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       namespace :current do
         resource :user, only: [:show]
       end
-      resource :articles, only: [:show]
+      resources :articles, only: [:show]
     end
   end
 end
