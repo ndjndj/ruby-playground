200e1ce7b1591df7ac5b2301906d34e5d0a05baa
ndjndj <teq.ndj@gmail.com>
2023-12-11 18:16:27 +0900

    f

--- rails_zenn/rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails_zenn/rails/app/controllers/api/v1/current/articles_controller.rb
@@ -1,2 +1,9 @@
-class Api::V1::Current::ArticlesController < ApplicationController
+class Api::V1::Current::ArticlesController < Api::V1::BaseController
+  before_action :authenticate_user!
+
+  def create
+    unsaved_article = current_user.articles.unsaved.first
+                   || current_user.articles.create!(status: :unsaved)
+    render json: unsaved_article
+  end
 end
