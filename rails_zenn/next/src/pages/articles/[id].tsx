931ebc58aa6244d87be3053a853f25cd1f9d8905
ndjndj <teq.ndj@gmail.com>
2023-12-12 16:56:22 +0900

    ｆ

--- rails_zenn/next/src/pages/articles/[id].tsx
+++ rails_zenn/next/src/pages/articles/[id].tsx
@@ -28,3 +28,32 @@ type ArticleProps = {
         name: string
     }
 }
+
+const ArticleDetail: NextPage = () => {
+    const router = useRouter() 
+    const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/articles/'
+    const { id } = router.query
+
+    const { data, error } = userSWR(id ? url + id : null, fetcher)
+    if (error) return <Error />
+    if (!data) return <Loading />
+
+    const article: ArticleProps = camelcaseKeys(data)
+
+    return (
+        <Box
+            sx={{
+                backgroundColor: '#EDF2F7',
+                pb: 6,
+                minHeight: 'calc(100vh - 57px)'
+            }}
+        >
+            <Box sx={{ pr: 1 }}>
+                <PersonIcon />
+            </Box>
+            <Box sx={{ mr: 2 }}>
+                <Typography component="p">著者:</Typography>
+            </Box>
+        </Box>
+    )
+}
