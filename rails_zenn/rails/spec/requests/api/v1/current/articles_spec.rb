7d5b43ce2e70044c2f549b7fcfb623d2a7673e41
ndjndj <teq.ndj@gmail.com>
2023-12-12 12:45:12 +0900

    f

--- rails_zenn/rails/spec/requests/api/v1/current/articles_spec.rb
+++ rails_zenn/rails/spec/requests/api/v1/current/articles_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Current::Articles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST api/v1/current/articles" do
+    subject { post(api_v1_current_articles_path, headers:) }
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { create(:user) }
+
+    context  "ログインユーザーに紐づく未保存ステータスの記事が 0 件のとき" do
+      it "未保存ステータスの記事が新規作成される" do
+        expect { subject }.to change { current_user.articles.count }.by(1)
+        expect(current_user.articles.last).to be_unsaved
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "from_today", "user"]
+        expect(res["user"].keys).to eq ["name"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    
   end
 end
