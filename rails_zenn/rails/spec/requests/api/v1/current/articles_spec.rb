fab8216805508cd97a601826b4dee4ebb7967d19
ndjndj <teq.ndj@gmail.com>
2023-12-12 13:22:30 +0900

    f

--- rails_zenn/rails/spec/requests/api/v1/current/articles_spec.rb
+++ rails_zenn/rails/spec/requests/api/v1/current/articles_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Current::Articles", type: :request do
+  describe "GET api/v1/current/articles" do
+    subject { get(api_v1_current_articles_path, headers:) }
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    before { create_list(:article, 2, user: other_user) }
+
+    context "ログインユーザーに紐づく articles レコードが存在するとき" do
+      before { create_list(:article, 3, user: current_user) }
+
+      it "正常にレコードを取得することができる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.length).to eq 3
+        expect(res[0].keys).to eq ["id", "title", "content", "status", "created_at", "from_today", "user"]
+        expect(res[0]["user"].keys).to eq ["name"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "ログインユーザーに紐づく articles レコードが存在しないとき" do
+      it "空の配列が返る" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res).to eq []
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
   describe "POST api/v1/current/articles" do
     subject { post(api_v1_current_articles_path, headers:) }
 
