42c721c6fa11143817d1e4f9047efda8d77f4a1c
ndjndj <teq.ndj@gmail.com>
2023-12-12 13:27:24 +0900

    f

--- rails_zenn/rails/spec/requests/api/v1/current/articles_spec.rb
+++ rails_zenn/rails/spec/requests/api/v1/current/articles_spec.rb
@@ -31,6 +31,27 @@ RSpec.describe "Api::V1::Current::Articles", type: :request do
         expect(response).to have_http_status(:ok)
       end
     end
+
+  end
+
+  describe "GET api/v1/current/articles/:id" do
+    subject { get(api_v1_current_article_path(id), headers:) }
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { create(:user) }
+
+    context ":id がログインユーザーに紐づく articles レコードの id であるとき" do
+      let(:current_user_article) { create(:article, user: current_user) }
+      let(:id) { current_user_article.id }
+
+      it "正常にレコードを取得することができる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "from_today", "user"]
+        expect(res["user"].keys).to eq ["name"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 
   describe "POST api/v1/current/articles" do
