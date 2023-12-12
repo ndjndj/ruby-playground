6527a8fc63f416cc9e227695ab1d93f2b6d77034
ndjndj <teq.ndj@gmail.com>
2023-12-12 16:06:02 +0900

    f

---
+++ rails_zenn/next/src/components/Loading.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material'
+import Image from 'next/image'
+import { styles } from '@/styles'
+
+const Loading = () => {
+    return (
+        <Box
+            css={styles.pageMinHeight}
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <Image 
+                src="/loading.svg" 
+                width={150} 
+                height={150} 
+                alt="loading..." 
+            />
+        </Box>
+    )
+}
