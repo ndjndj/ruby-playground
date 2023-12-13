d76137d75618b1534c5baad68c39fa1f8396acf6
ndjndj <teq.ndj@gmail.com>
2023-12-13 17:56:48 +0900

    f

---
+++ rails_zenn/next/src/components/CurrentUserFetch.tsx
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse, AxiosError } from 'axios'
+import { useEffect } from 'react'
+import { useUserState } from '@/hooks/useGlobalState'
+
+const CurrentUserFetch = () => {
+    const [user, setUser] = useUserState()
+
+    const _get = (k: string): string => {
+        return localStorage.getItem(k) ?? ""
+    }
+
+    useEffect(() => {
+        if (user.isFetched) {
+            return 
+        }
+
+        if (localStorage.getItem('access-token')) {
+            const url = process.env.NEXT_PUBLIC_API_BASE_URL 
+                      + '/current/user'
+            
+            axios
+            .get(
+                url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'access-token': _get('access-token'),
+                    client: _get('client'),
+                    uid: _get('uid')
+                }
+            })
+        }
+    })
+}
