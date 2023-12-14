5e26318ab1aa4985f8b544c785455a125f1d78f4
ndjndj <teq.ndj@gmail.com>
2023-12-14 18:35:36 +0900

    f

---
+++ rails_zenn/next/src/pages/sign_up.tsx
@@ -0,0 +1,87 @@
+import { LoadingButton } from '@mui/lab'
+import { Box, Container, TextField, Typography, Stack } from '@mui/material'
+import axios, { AxiosResponse, AxiosError } from 'axios'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
+import { useSnackbarState } from '@/hooks/useGlobalState'
+import { styles } from '@/styles'
+
+type SignUpFormData = {
+    email: string 
+    password: string 
+    name: string
+}
+
+const SignUp: NextPage = () => {
+    const router = useRouter() 
+    const [, setSnackbar] = useSnackbarState() 
+    const [isLoading, setIsLoading] = useState(false)
+
+    const { handleSubmit, control } = useForm<SignUpFormData>({
+        defaultValues: { email: '', password: '' }
+    })
+
+    const validationRules = {
+        email: {
+            required: 'メールアドレスを入力してください。',
+            pattern: {
+                value: /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                message: '正しい形式のメールアドレスを入力してください'
+            }
+        },
+        password: {
+            required: 'パスワードを入力してください'
+        },
+        name: {
+            required: 'ユーザー名を入力してください'
+        }
+    }
+
+    const onSubmit: SubmitHandler<SignUpFormData> = (data) => {
+        const SignUp = async (data: SignUpFormData) => {
+            setIsLoading(true)
+            const base = process.env.NEXT_PUBLIC_FRONT_BASE_URL 
+            const url = base + '/auth'
+            const headers = { 'Content-Type': 'application/json' }
+            const confirmSuccessUrl = base + '/sign_in'
+            
+            await axios({
+                method: 'POST', 
+                url: url,
+                data: { ...data, confirm_success_url: confirmSuccessUrl },
+                headers: headers
+            }).then((res: AxiosResponse) => {
+                localStorage.setItem(
+                    'access-token',
+                    res.headers['access-token'] || '',
+                )
+                localStorage.setItem(
+                    'client', 
+                    res.headers['client'] || '' 
+                )
+                localStorage.setItem(
+                    'uid',
+                    res.headers['uid'] || ''
+                )
+                setSnackbar({
+                    message: '認証メールをご確認ください',
+                    severity: 'success',
+                    pathname: '/'
+                })
+                router.push('/')
+            }).catch((e: AxiosError<{ error: string }>) => {
+                console.log(e.message)
+                setSnackbar({
+                    message: '不正なユーザー情報です',
+                    severity: 'error', 
+                    pathname: '/sign_up'
+                })
+                setIsLoading(false)
+            })
+        }
+
+        SignUp(data)
+    }
+}
