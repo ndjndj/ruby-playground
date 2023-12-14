829f44eadc11052b92395fa34b6d6fdf52f91eb7
ndjndj <teq.ndj@gmail.com>
2023-12-14 11:15:29 +0900

    f

--- rails_zenn/next/src/hooks/useRequireSignedIn.ts
+++ rails_zenn/next/src/hooks/useRequireSignedIn.ts
@@ -2,3 +2,19 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useUserState, useSnackbarState } from '@/hooks/useGlobalState'
 
+export function useRequireSignedIn() {
+    const router = useRouter() 
+    const [user] = useUserState() 
+    const [, setSnackbar] = useSnackbarState() 
+
+    useEffect(() => {
+        if (user.isFetched && !user.isSignedIn) {
+            setSnackbar({
+                message: 'サインイン', 
+                severity: 'error',
+                pathname: '/sign_in'
+            })
+            router.push('/sign_in')
+        }
+    }, [user, router, setSnackbar])
+}
